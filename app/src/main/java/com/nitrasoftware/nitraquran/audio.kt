0ae65715ac4f73509b5d46e6d9ee88a878393811
Tecnologico005 <studenttecno@outlook.es>
2026-06-10 20:56:03 +0200

    Added an experimental media player and created the raw folder"

---
+++ app/src/main/java/com/nitrasoftware/nitraquran/audio.kt
@@ -0,0 +1,10 @@
+import android.content.Context
+import android.media.MediaPlayer
+
+fun reproducir(context: Context, archivoResId: Int) {
+val mediaPlayer = MediaPlayer.create(context, archivoResId)
+mediaPlayer.start()
+mediaPlayer.setOnCompletionListener {
+it.release()
+}
+
